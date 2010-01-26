ceb3a4f618ca42e59830244795d9dcebf174b192
David N. Welton <davidw@catlow.(none)>
2010-01-26 22:12:58 +0100

    Require diff-lcs gem.

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,8 @@ Rails::Initializer.run do |config|
   # Only load the plugins named here, by default all plugins in vendor/plugins are loaded
   # config.plugins = %W( exception_notification ssl_requirement )
 
+  config.gem 'diff-lcs', :lib => 'diff/lcs'
+
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
